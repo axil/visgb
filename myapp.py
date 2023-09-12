7df8f2cd2e69bc673915faa5cece83addaedfb54
Lev Maximov <lev.maximov@gmail.com>
2023-09-12 17:58:29 +0700

    catboost and xgboost work

--- myapp.py
+++ myapp.py
@@ -1,5 +1,7 @@
-import numpy as np
+from collections import defaultdict
+from math import exp
 
+import numpy as np
 from bokeh.plotting import figure
 import bokeh.models as bm
 from bokeh.models import ColumnDataSource, Column
@@ -27,14 +29,18 @@ def update(tab=None):
 #    tab = self.tabs.currentWidget().objectName()
     if tab is None:
         tab = tabs.tabs[tabs.active].name
-    if tab == 'xgboost':
-        for i in range(xgb_averaging.value):
-            model = XGBClassifier(
-                n_estimators=xgb_n_estimators.value, 
-                #subsample=self.subsample.value(), 
-                random_state=xgb_random_state.value + i,
-                #max_depth=self.max_depth.value()
-            )
+    if tab == 'xgb':
+        for i in range(params[tab]['averaging'].value):
+            kwargs = {k: v.value for k, v in params[tab].items() if k != 'averaging'}
+            for name in 'alpha', 'lambda', 'gamma':
+                kwargs[name] = exp(kwargs[name])
+            kwargs['random_state'] += i
+            model = XGBClassifier(**kwargs)
+#                n_estimators=xgb_n_estimators.value, 
+#                #subsample=self.subsample.value(), 
+#                random_state=xgb_random_state.value + i,
+#                #max_depth=self.max_depth.value()
+#            )
             print(f'update {i}')
 #            print('X =', X)
 #            print('y =', y)
@@ -42,13 +48,15 @@ def update(tab=None):
 #            print('X1 =', X1[:10])
             preds = model.predict_proba(X1)[:,0].reshape(xgrid.shape)
             all_preds.append(preds)
-    elif tab == 'catboost':
-        for i in range(cat_averaging.value):
-            model = CatBoostClassifier(
-                num_trees=cat_num_trees.value, 
-                depth=cat_depth.value, 
-                random_seed=cat_random_seed.value + i,
-            )
+    elif tab == 'cat':
+        for i in range(params[tab]['averaging'].value):
+            kwargs = {k: v.value for k, v in params[tab].items() if k != 'averaging'}
+            kwargs['random_seed'] += i
+            model = CatBoostClassifier(**kwargs)
+#                num_trees=cat_num_trees.value, 
+#                depth=cat_depth.value, 
+#                random_seed=cat_random_seed.value + i,
+#            )
             model.fit(X, y)
             preds = model.predict_proba(X1)[:,0].reshape(xgrid.shape)
             all_preds.append(preds)
@@ -70,8 +78,8 @@ def update(tab=None):
     print('done')
 
 TOOLS = "tap"
-p = figure(title='tap = blue dot, shift-tap = orange dot',
-           tools=TOOLS, width=300, height=300,
+p = figure(title='tap=blue dot, shift-tap=orange dot; min 8 dots',
+           tools=TOOLS, width=350, height=350,
            x_range=(0, 100), y_range=(0, 100))
 
 source1 = ColumnDataSource(data=dict(x=[], y=[]))   
@@ -87,65 +95,41 @@ im = p.image('image', source=source0, x=0, y=0, dw=100, dh=100, level="image")#,
 def slider_callback(attr, old, new):
     update()
 
-
 params_dict = {
     'xgb': {
         'n_estimators': {'start': 1, 'end': 100, 'value': 10},
+        'subsample':    {'start': 0.,'end': 1.,  'value': 1., 'step': 0.1},
+        'alpha':        {'start': -16, 'end': 2,  'value': -16},
+        'lambda':       {'start': -16, 'end': 2,  'value': 0},
+        'gamma':        {'start': -16, 'end': 2,  'value': -16},
         'random_state': {'start': 1, 'end': 100, 'value': 10},
-        'averaging':    {'start': 1, 'end': 20,  'value': 1},
+        'averaging':    {'start': 1, 'end': 10,  'value': 1},
     },
     'cat': {
         'num_trees':    {'start': 1, 'end': 100, 'value': 10},
+        'l2_leaf_reg':  {'start': 1, 'end': 10,  'value': 3},
         'random_seed':  {'start': 1, 'end': 100, 'value': 10},
-        'averaging':    {'start': 1, 'end': 20,  'value': 1},
+        'averaging':    {'start': 1, 'end': 10,  'value': 1},
     }
 }
         
-
-#params = defaultdict(dict)
-#tab_contents = {}
-#for model, pp in params_dict:
-#    for k, v in pp:
-#        v['title'] = k
-#        params[model][k] = bm.Slider(**v)
-#    tab_contents[model] = bl.Column(params[model].values)
-#
-xgb_max_depth = bm.Slider(start=1, end=10, value=3, title="max_depth")
-xgb_max_depth.on_change('value', slider_callback)
-xgb_n_estimators = bm.Slider(start=1, end=100, value=10, title="n_estimators")
-xgb_n_estimators.on_change('value', slider_callback)
-xgb_random_state = bm.Slider(start=1, end=100, value=10, title="random_state")
-xgb_random_state.on_change('value', slider_callback)
-xgb_averaging = bm.Slider(start=1, end=20, value=1, title="averaging")
-xgb_averaging.on_change('value', slider_callback)
-
-cat_num_trees = bm.Slider(start=1, end=100, value=10, title="num_trees")
-cat_num_trees.on_change('value', slider_callback)
-cat_depth = bm.Slider(start=1, end=16, value=7, title="depth")
-cat_depth.on_change('value', slider_callback)
-cat_random_seed = bm.Slider(start=1, end=100, value=10, title="random_seed")
-cat_random_seed.on_change('value', slider_callback)
-cat_averaging = bm.Slider(start=1, end=20, value=1, title="averaging")
-cat_averaging.on_change('value', slider_callback)
+params = defaultdict(dict)
+tab_contents = {}
+for model, pp in params_dict.items():
+    for name, kwargs in pp.items():
+        kwargs['title'] = name
+        params[model][name] = bm.Slider(**kwargs)
+        params[model][name].on_change('value', slider_callback)
+    tab_contents[model] = bl.Column(*list(params[model].values()))
 
 # _____________ tabs _______________ 
-xgb_tab = bm.TabPanel(child=bl.Column(
-    xgb_max_depth,
-    xgb_n_estimators,
-    xgb_random_state,
-    xgb_averaging,
-), title="XGBoost", name='xgboost')
-cat_tab = bm.TabPanel(child=bl.Column(
-    cat_num_trees,
-    cat_depth,
-    cat_random_seed,
-    cat_averaging,
-), title="CatBoost", name='catboost')
+xgb_tab = bm.TabPanel(child=tab_contents['xgb'], title="XGBoost", name='xgb')
+cat_tab = bm.TabPanel(child=tab_contents['cat'], title="CatBoost", name='cat')
 
 cat1x, cat1y = [], []
 cat2x, cat2y = [], []
 
-def callback(event):
+def on_tap(event):
     class_id = active_class
     if event.modifiers['shift']:
         class_id = 3 - class_id
@@ -159,7 +143,7 @@ def callback(event):
         source2.data = dict(x=cat2x, y=cat2y)
     update()
 
-p.on_event(Tap, callback)
+p.on_event(Tap, on_tap)
 
 def panelActive(attr, old, new):
     name = tabs.tabs[new].name
@@ -183,17 +167,30 @@ def select_class(class_id):
             active_class = 3-active_class
     return wrapped
 
+def on_reset():
+    cat1x.clear()
+    cat1y.clear()
+    cat2x.clear()
+    cat2y.clear()
+    source1.data = dict(x=cat1x, y=cat1y)
+    source2.data = dict(x=cat2x, y=cat2y)
+    blue.update(active=True)
+    update()
+
 # __________ class buttons _______________
 blue = bm.Toggle(label='Class 1', active=True)
 orange = bm.Toggle(label='Class 2')
 blue.on_click(select_class(1))
 orange.on_click(select_class(2))
 
+reset = bm.Button(label='Reset')
+reset.on_click(on_reset)
+
 tabs = bm.Tabs(tabs=[xgb_tab, cat_tab])
 tabs.on_change('active', panelActive)
 
 curdoc().add_root(bl.Column(
     p,
-    bl.Row(blue, orange),
+    bl.Row(blue, orange, reset),
     tabs,
 ))
